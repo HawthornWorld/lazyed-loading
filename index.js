cfcefe7ae9729f8fb56339c1c05a59b43343aa74
马燕 <mosic.ma@hotmail.com>
2017-07-27 15:26:05 +0800

    update

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 /**
  * Created by mayan on 2017/7/21.
  */
+function lazyload() {
+    var images = document.getElementsByTagName('img');
+    var n = 0;
+    return function () {
+        var scrollTop = document.documentElement.scrollTop||document.body.scrollTop;
+        var viewHeight = document.documentElement.clientHeight;
+        for(var i = n; i < n; i++){
+            if(images[i].offset<scrollTop+viewHeight){
+                if(images[i].getAttribute('src') === 'images/loading'){
+                    images[i].src = images[i].getAttribute('data-original');
+                }
+                n = n+1;
+            }
+        }
+    }
+}
+var loadImage = lazyload();
+loadImage();
+window.addEventListener('scroll',loadImage,false);
