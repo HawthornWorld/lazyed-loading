d65da6c23ecadbe61f1b9c5623d8da3d5b304f56
马燕 <mosic.ma@hotmail.com>
2017-07-27 22:49:01 +0800

    update

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
 /**
  * Created by mayan on 2017/7/21.
  */
+function throttle(fn, delay, atleast) {
+    var timeout = null,
+        startTime = new Date();
+    return function() {
+        var curTime = new Date();
+        clearTimeout(timeout);
+        if(curTime - startTime >= atleast) {
+            fn();
+            startTime = curTime;
+        }else {
+            timeout = setTimeout(fn, delay);
+        }
+    }
+}
+var n = 0;
+var images = document.getElementsByTagName('img');
 function lazyload() {
-    var images = document.getElementsByTagName('img');
-    var n = 0;
     return function () {
         var scrollTop = document.documentElement.scrollTop||document.body.scrollTop;
         var viewHeight = document.documentElement.clientHeight;
-        for(var i = n; i < n; i++){
-            if(images[i].offset<scrollTop+viewHeight){
-                if(images[i].getAttribute('src') === 'images/loading'){
+        for(var i = n; i < images.length; i++){
+            if(images[i].offsetTop < scrollTop + viewHeight){
+                if(images[i].getAttribute('src') === 'images/loading.gif'){
                     images[i].src = images[i].getAttribute('data-original');
                 }
-                n = n+1;
+                n++;
             }
         }
     }
 }
-var loadImage = lazyload();
-loadImage();
-window.addEventListener('scroll',loadImage,false);
+var loadImages = lazyload();
+loadImages();
+window.addEventListener('scroll', throttle(loadImages, 500, 1000), false);
